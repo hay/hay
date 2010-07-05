e6f2e8e487c2b67d743c0e2f182a4de509fae59d
Hay <hay@ip169-107.vpro.nl>
2010-07-05 15:29:01 +0200

    Added documentation

--- jsloader/jsloader.js
+++ jsloader/jsloader.js
@@ -1,3 +1,28 @@
+/*
+ * loadScript() - A basic Javascript loader with support for arrays
+ * - http://github.com/hay/hay/tree/master/jsloader/
+ * Based on Nicholas C. Zakas' script:
+ * - http://www.nczonline.net/blog/2009/07/28/the-best-way-to-load-external-javascript/
+ * Licensed under the MIT / X11 License:
+ * - http://opensource.org/licenses/mit-license.php
+ *
+ * USAGE
+ * You can either load a single script (just provide the URL) or an array
+ * of scripts. You can then provide a callback for your 'init' function
+ *
+ * - Single script:
+ * loadScript("http://example.com/myscript.js", function() {
+ *     alert('loaded!');
+ * });
+ *
+ * - Multiple scripts:
+ * var scripts = ['script1.js', 'script2.js', 'script3.js'];
+ * loadScript(scripts, function() {
+ *     alert("All scripts loaded!");
+ * });
+ *
+**/
+
 function loadScript(arg, cb) {
     function load(url, callback) {
         var script = document.createElement("script")
