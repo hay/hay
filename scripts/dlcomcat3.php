9593b506325c5427c9aab3dc4cd2d41233635fc9
Hay <huskyr@gmail.com>
2010-11-11 02:02:51 +0100

    adding new downloaders for commons

---
+++ scripts/dlcomcat3.php
@@ -0,0 +1,40 @@
+#!/usr/bin/php
+<?php
+require 'dlcomcatcm.php';
+
+$files = file("sebas2.txt");
+
+function request($title) {
+    $mw = new MwApiRequest(array(
+        "format" => "json",
+        "action" => "query",
+        "titles" => $title,
+        "prop" => "imageinfo",
+        "iiprop" => "url",
+        "indexpageids" => "1"
+    ));
+
+    return $mw->getResponse();
+}
+
+function logit($msg) {
+    file_put_contents("dllog.log", "$msg \n", FILE_APPEND);
+}
+
+$images = array();
+foreach ($files as $f) {
+    logit("GET: " . $f);
+
+    $r = request($f);
+    $pid = $r['query']['pageids'][0];
+    $url = $r['query']['pages'][$pid]['imageinfo'][0]['url'];
+
+    $images[] = $url;
+    file_put_contents("imageurl.json", json_encode($images));
+
+    logit("FOUND: $url");
+
+    // Download
+    exec('wget -P jpg -nc "' . $url . '"');
+}
+logit("DONE");
